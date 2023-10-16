a839750ec6ddac5838ca86a6816494e812bffc3f
erneto <128459472+erneto13@users.noreply.github.com>
2023-10-15 21:53:50 -0700

    nose que mamada hice

---
+++ ejecutarApps-c#/lp-socketsAppRemotas-cliente/main-cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            // Creamos el objeto cliente y le asignamos la IP
+            TcpClient cliente = new TcpClient("0.0.0.0", 8000);
+
+            // Flujo de red pa' comunicarse al server
+            NetworkStream stream = cliente.GetStream();
+
+            Console.WriteLine("Aplicaciones remotas disponibles:");
+            Console.WriteLine("1. Excel");
+            Console.WriteLine("2. PowerPoint");
+            Console.WriteLine("3. Bloc de Notas");
+
+            // Se le pide al cliente que seleccione una opcion y se almacena en una cadena
+            Console.Write("Seleccione la aplicación (1/2/3): ");
+            string op = Console.ReadLine();
+
+            // Convertimos la elección del usuario a un arreglo de bytes y lo enviamos al servidor.
+            byte[] infoBytes = System.Text.Encoding.ASCII.GetBytes(op);
+            stream.Write(infoBytes, 0, infoBytes.Length);
+
+            // Se cierra la conexión
+            cliente.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error al conectar al servidor: " + ex.Message); // Error por si el cliente no esta disponible (encender el server primero)
+        }
+    }
+}
---
+++ ejecutarApps-c#/lp-socketsAppRemotas-servidor/main-servidor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Threading;
+
+class Server
+{
+    static void Main()
+    {
+        // Se crea un objeto para escuchar conexiones
+        TcpListener elEscuchador = new TcpListener(IPAddress.Any, 8000);
+        elEscuchador.Start();
+        Console.WriteLine("Servidor esperando conexiones...");
+
+        while (true) // Ciclo para detectar clientes
+        {
+            // Aceptamos la entrada de un cliente
+            TcpClient cliente = elEscuchador.AcceptTcpClient();
+            Console.WriteLine("Cliente conectado");
+
+            // Se crea un hilo para administrar las entradas del cliente
+            Thread hiloCliente = new Thread(HandleClient);
+            hiloCliente.Start(cliente);
+        }
+    }
+
+    static void HandleClient(object clientObj)
+    {
+        TcpClient cliente = (TcpClient)clientObj;
+        NetworkStream stream = cliente.GetStream();
+
+        // Búfer para leer la solicitud del cliente.
+        byte[] buffer = new byte[1024];
+
+        // Se lee la solicitud del cliente y se convierte a cadena
+        int lectorBits = stream.Read(buffer, 0, buffer.Length);
+        string peticionCliente = System.Text.Encoding.ASCII.GetString(buffer, 0, lectorBits);
+
+        Console.WriteLine("Solicitud recibida: " + peticionCliente);
+
+        // Se abre un switch para las opciones que el cliente pidio
+        switch (peticionCliente)
+        {
+            case "1":
+                StartApplication("excel.exe");
+                break;
+            case "2":
+                StartApplication("powerpnt.exe");
+                break;
+            case "3":
+                StartApplication("notepad.exe");
+                break;
+            default:
+                Console.WriteLine("Solicitud no válida");
+                break;
+        }
+
+        // Cerramos la conexión con el cliente
+        cliente.Close();
+    }
+
+    static void StartApplication(string app)
+    {
+        try
+        {
+            // Con el proceso Start se abre la app que pidio el cliente
+            Process.Start(app);
+            Console.WriteLine("Aplicación ejecutada: " + app);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error al ejecutar la aplicación: " + ex.Message); // Cachar cualquier error por si la app no jala o no existe o quien sabe lo que caiga es bueno
+        }
+    }
+}
